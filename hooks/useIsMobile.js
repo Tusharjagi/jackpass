f1d04b12153a37ff77984c65fb1d9bf111c1f37e
Tusharjagi <tusharjagi@gmail.com>
2025-02-02 17:59:04 +0530

    update useIsMobile custom hook

--- hooks/useIsMobile.js
+++ hooks/useIsMobile.js
@@ -1,20 +1,19 @@
-"use client";
-
 import { useState, useEffect } from "react";
 
 const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    if (typeof window === "undefined") return;
+
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    window.addEventListener("resize", handleResize);
+    handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return isMobile;
