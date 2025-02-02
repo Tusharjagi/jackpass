f22313070cd66af6c980b15688c691084c06ca52
Tusharjagi <tusharjagi@gmail.com>
2025-02-02 18:02:38 +0530

    description comp

--- components/add-description/index.jsx
+++ components/add-description/index.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
 import { ChevronRight, PencilLine } from "lucide-react";
+import { Textarea } from "../ui/textarea";
+import { useEvent } from "@/store/EventContext";
 
 export default function AddDescription() {
+  const { setEvent } = useEvent();
+  const [isClickOnDescription, setIsClickOnDescription] = useState(false);
+
+  const handleDescriptionClick = () => {
+    setIsClickOnDescription(true);
+  };
+
+  const handleOnBlur = (event) => {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      setIsClickOnDescription(false);
+    }
+  };
+
+  const handleOnChange = (event) => {
+    setEvent("description", event.target.value);
+  };
+
   return (
-    <div className="flex gap-2 cursor-pointer items-center ">
+    <div
+      className="flex gap-2 cursor-pointer items-center"
+      onClick={handleDescriptionClick}
+      onBlur={handleOnBlur}
+      tabIndex={0}
+    >
       <PencilLine className="text-gray-600" />
       <div className="flex justify-between w-full">
         <div>
           <h2>
             Add Description <span className="text-red-600">*</span>
           </h2>
-          <span className="text-xs text-gray-600">
-            Add a brief description to let attendees know what your event all
-            about
-          </span>
+          {isClickOnDescription ? (
+            <Textarea
+              placeholder="Type your message here."
+              className="w-[280px]"
+              onChange={handleOnChange}
+            />
+          ) : (
+            <span className="text-xs text-gray-600">
+              Add a brief description to let attendees know what your event all
+              about
+            </span>
+          )}
         </div>
         <span>
           <ChevronRight className="text-gray-600" />
